b7cb34ef89bc49e276a294c41395fefe00ae1f57
manitripathi <manitripathi.023@gmail.com>
2021-09-25 16:33:44 +0530

    msg

--- main.js
+++ main.js
@@ -17,3 +17,56 @@ function time() {
      aMpM.innerHTML     = d.getHours() >= 12 ? "PM" : "AM"; 
 }
 setInterval(time, 1000);
+
+//Party img change
+const partyImg=document.querySelector('#party');
+partyImg.addEventListener('click',function(){
+  boxImg.style.backgroundImage="url(./party2.svg)"; 
+  changeText.innerText="LET'S PARTY !!";
+});
+
+//Change image with by clicking on options
+const changeImg1=document.querySelector('#morningImg');
+changeImg1.addEventListener('click',function(){
+  boxImg.style.backgroundImage="url(./3.png)";
+  changeText.innerText="WAKE UP, EAT BREAKFAST !!";
+  textReplace.innerText="GOOD MORNING !!";
+});
+
+const changeImg2=document.querySelector('#afternoonImg');
+changeImg2.addEventListener('click',function(){
+  boxImg.style.backgroundImage="url(./home.png)";
+  changeText.innerText="LET'S HAVE SOME LUNCH !!";
+  textReplace.innerText="GOOD AFTERNOON !!";
+});
+
+const changeImg3=document.querySelector('#nightImg');
+changeImg3.addEventListener('click',function(){
+  boxImg.style.backgroundImage="url(./2.png)";
+  changeText.innerText="LET'S SLEEP !!";
+  textReplace.innerText="GOOD NIGHT !!";
+});
+
+//Images changes with Time
+var d = new Date();
+var h = d.getHours(); 
+if(h>=5 && h<12){
+  boxImg.style.backgroundImage="url(./3.png)";
+  changeText.innerText="WAKE UP, EAT BREAKFAST !!";
+  textReplace.innerText="GOOD MORNING !!";
+}
+else if(h>=12 && h<18){
+  boxImg.style.backgroundImage="url(./home.png)";
+  changeText.innerText="LET'S HAVE SOME LUNCH !!";
+  textReplace.innerText="GOOD AFTERNOON !!";
+}
+else if(h>=18 && h<23){
+  boxImg.style.backgroundImage="url(./2.png)";
+  changeText.innerText="LET'S SLEEP !!";
+  textReplace.innerText="GOOD NIGHT !!";
+}
+else{
+  boxImg.style.backgroundImage="url(./party2.svg)"; 
+  changeText.innerText="LET'S PARTY !!";
+  document.getElementById("textReplace").style.display="none";
+}
